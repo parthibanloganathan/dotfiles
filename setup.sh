23577d4ac85b6091c6b1a74d6b20902be2331858
Parthiban Loganathan <pl2487@columbia.edu>
2014-09-07 16:17:44 -0400

    Update setup for ohmyzsh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,10 @@ ln -s $DIRECTORY/.zshr_functions
 ln -s $DIRECTORY/.vimrc
 ln -s $DIRECTORY/.tmux.conf
 ln -s $DIRECTORY/.gitconfig
+
+# Install ohmyzsh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+cp $DIRECTORY/parthi1.zsh-theme ~/.oh-my-zsh/themes/
+cd ~
+source .zshrc
 echo Done
